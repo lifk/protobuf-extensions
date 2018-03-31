c354e859189f20297549de464199320b2a85184e
Lifk <luisbcr@gmail.com>
2018-03-31 18:50:04 +0200

    Initial commit

---
+++ src/main/kotlin/es/lifk/protobufextensions/ProtobufExtensions.kt
@@ -0,0 +1,13 @@
+package es.lifk.protobufextensions
+
+import com.google.protobuf.Any
+import com.google.protobuf.Struct
+import com.google.protobuf.Value
+
+class WellKnownTypesBuilders {
+    inline fun Any(block: Any.Builder.() -> Unit) = Any.newBuilder().apply(block).build()
+
+    inline fun Struct(block: Struct.Builder.() -> Unit) = Struct.newBuilder().apply(block).build()
+
+    inline fun Value(block: Value.Builder.() -> Unit) = Value.newBuilder().apply(block).build()
+}
