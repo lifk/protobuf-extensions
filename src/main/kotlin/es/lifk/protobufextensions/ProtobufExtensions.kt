c2a5e49356b04d2e269cdc91b22c0ca926af83da
Lifk <luisbcr@gmail.com>
2018-04-09 20:17:21 +0200

    Added copy methods

--- src/main/kotlin/es/lifk/protobufextensions/ProtobufExtensions.kt
+++ src/main/kotlin/es/lifk/protobufextensions/ProtobufExtensions.kt
@@ -6,8 +6,10 @@ import com.google.protobuf.Value
 
 object WellKnownTypesBuilders {
     inline fun Any(block: Any.Builder.() -> Unit) = Any.newBuilder().apply(block).build()
-
     inline fun Struct(block: Struct.Builder.() -> Unit) = Struct.newBuilder().apply(block).build()
-
     inline fun Value(block: Value.Builder.() -> Unit) = Value.newBuilder().apply(block).build()
 }
+
+fun Any.copy(block: Any.Builder.() -> Unit) = this.toBuilder().apply(block).build()
+fun Struct.copy(block: Struct.Builder.() -> Unit) = this.toBuilder().apply(block).build()
+fun Value.copy(block: Value.Builder.() -> Unit) = this.toBuilder().apply(block).build()
