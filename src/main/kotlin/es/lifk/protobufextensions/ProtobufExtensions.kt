6fdb17f52a98df48b0b497c46c67128c58b0ee24
Lifk <luisbcr@gmail.com>
2018-04-25 18:50:30 +0200

    Added additional methods to Value and Struct

--- src/main/kotlin/es/lifk/protobufextensions/ProtobufExtensions.kt
+++ src/main/kotlin/es/lifk/protobufextensions/ProtobufExtensions.kt
@@ -13,3 +13,19 @@ object WellKnownTypesBuilders {
 fun Any.copy(block: Any.Builder.() -> Unit) = this.toBuilder().apply(block).build()
 fun Struct.copy(block: Struct.Builder.() -> Unit) = this.toBuilder().apply(block).build()
 fun Value.copy(block: Value.Builder.() -> Unit) = this.toBuilder().apply(block).build()
+
+fun Value.getAnyValue(): kotlin.Any? {
+    return when (kindCase ?: Value.KindCase.NULL_VALUE) {
+        Value.KindCase.NULL_VALUE -> null
+        Value.KindCase.NUMBER_VALUE -> numberValue
+        Value.KindCase.STRING_VALUE -> stringValue
+        Value.KindCase.BOOL_VALUE -> boolValue
+        Value.KindCase.STRUCT_VALUE -> structValue
+        Value.KindCase.LIST_VALUE -> listValue
+        Value.KindCase.KIND_NOT_SET -> null
+    }
+}
+
+fun Struct.fieldsMapToAny(): Map<String, kotlin.Any?> {
+    return fieldsMap.mapValues { (_, value) ->  value.getAnyValue()}
+}
